1fd8e159487961b2de1113e1bf556fe989e42157
CanaanGM <reakak1@gmail.com>
2023-06-12 17:34:01 +0300

    basic routes

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 - docs are outdated, so looking for a way to customize the port took a while
 - unrelated to Rocket, but rust analyzer blocks a resource so u wont be able to build till it finishes.
 
-
+- you can easily split routes depending on ur needs, same with error catchers
+  - error catchers are like a middle ware that handles status code errors, i would think
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,64 @@
 extern crate rocket;
 
 use rocket::config::Config;
+use rocket::response::status;
+use rocket::serde::json::{json, Value};
 
 #[get("/")]
-fn handle_root() -> &'static str {
-    "Hello !!"
+fn handle_root() -> Value {
+    json!("Greetings~!\n")
+}
+
+#[catch(404)]
+fn book_not_found() -> Value {
+    json!("Book Not Found . . . ")
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!("Not Found . . . ")
+}
+
+#[get("/")]
+fn get_books() -> Value {
+    json!(
+    [
+        {"id":1, "name":"Horus Rising", "category":"science fiction"},
+        {"id":2, "name":"Designing data intensive applications", "category":"computer science"}
+        ]
+    )
+}
+
+#[get("/<id>")]
+fn get_book_by_id(id: i32) -> Value {
+    json!(
+        {"id":id, "name":"always the same id?", "category":"always has been"}
+    )
+}
+
+#[post("/", format = "json")]
+fn create_book() -> Value {
+    json!(
+        {"id":3, "name":"Filgrim", "category":"science fiction"}
+    )
+}
+
+#[put("/<id>", format = "json")]
+fn update_book(id: i32) -> Value {
+    json!(
+        {"id":id, "name":"Updated", "category":"jokes on yee!"}
+    )
+}
+
+#[delete("/<id>")]
+fn delete_book(id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
-    let port = 9789; // Custom port number
+    let port = 9789;
 
-    // Create a custom configuration with the desired port
     let config = Config {
         port: port,
         ..Config::default()
@@ -20,6 +67,18 @@ async fn main() {
 
     let _ = rocket::custom(config)
         .mount("/", routes![handle_root])
+        .register("/", catchers![not_found]) // default catcher
+        .mount(
+            "/books/",
+            routes![
+                get_books,
+                get_book_by_id,
+                update_book,
+                delete_book,
+                create_book
+            ],
+        )
+        .register("/books", catchers![book_not_found]) // custom catcher
         .launch()
         .await;
 }
